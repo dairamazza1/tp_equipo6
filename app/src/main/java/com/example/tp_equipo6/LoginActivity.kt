bd85c18c0d73511a112de67d8660a17708c547c4
thiago-04 <thiagojavier2004@gmail.com>
2024-09-08 18:28:36 -0300

    Login integrado

---
+++ app/src/main/java/com/example/tp_equipo6/LoginActivity.kt
@@ -0,0 +1,73 @@
+package com.example.tp_equipo6
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var etUsuario: EditText
+    lateinit var etPassword: EditText
+    lateinit var cbRecordarUsuario: CheckBox
+    lateinit var btnRegistrarse: Button
+    lateinit var btnIniciarSesion: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        etUsuario = findViewById(R.id.etUsuario)
+        etPassword = findViewById(R.id.etPassword)
+        cbRecordarUsuario = findViewById(R.id.cbRecordarUsuario)
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion)
+
+        btnRegistrarse.setOnClickListener{
+
+            val intent= Intent(this, registro::class.java)
+            startActivity(intent)
+        }
+
+        btnIniciarSesion.setOnClickListener{
+
+            //Texto que ingresa el usuario.
+            var usuario= etUsuario.text.toString()
+
+            //Pregunto si alguna de las dos cosas esta vacia.
+            if(usuario.isEmpty() || etPassword.text.toString().isEmpty() ){
+
+                var mensaje= "Completar Datos"
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+            } else{
+
+                if(cbRecordarUsuario.isChecked){
+
+                    Log.i("TODO", "Funcionalidad de recordar Usuario y Contraseña")
+                }
+
+                val intent= Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+        }
+
+
+    }
+}
